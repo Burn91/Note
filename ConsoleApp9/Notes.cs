28e75ce922610a2b0e1961d51719d894383e5591
Burn91 <47191830+Burn91@users.noreply.github.com>
2022-02-19 14:03:00 +0100

    Add files via upload

---
+++ ConsoleApp9/Notes.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp9
+{
+	public class Notes
+	{
+	
+		string Note { get; set; }
+		DateTime NoteDate { get; set; }
+
+		public Notes(string note, DateTime noteDate)
+		{
+			Note = note;
+			NoteDate = noteDate;
+		}
+
+		public override string ToString()
+		{
+		
+			return $"######\n{Note}\n{NoteDate}";
+		}
+
+
+	}
+}
